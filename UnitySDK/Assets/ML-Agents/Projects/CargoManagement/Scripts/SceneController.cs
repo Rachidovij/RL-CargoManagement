cf219e6cac4c53f5068a281ac989112c2f8da8d6
Rachid Oucheikh <rachid.oucheikh@ju.se>
2020-11-26 17:51:52 +0100

    Initial Commit

---
+++ UnitySDK/Assets/ML-Agents/Projects/CargoManagement/Scripts/SceneController.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SceneController : MonoBehaviour
+{
+
+	public class SpawnLocation {
+
+		public bool used;
+		public Transform transform;
+		public LandOwner landOwner;
+
+	}
+
+	public enum LandOwner {
+		None,
+		CargoArea
+	}
+
+	[SerializeField] private List<GameObject> CargoAreas;
+	[SerializeField] private List<GameObject> Cargoes;
+    [SerializeField] private List<GameObject> CargoToSpawn;
+    public List<GameObject> spawnedCargoes;
+	[SerializeField] private GameObject spawnPoints;
+	[SerializeField] private List<SpawnLocation> spawnLocations = new List<SpawnLocation>();
+	[SerializeField] private TagMaster agent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform spawnpoint in spawnPoints.transform) {
+        	spawnLocations.Add(new SpawnLocation {used = false, transform=spawnpoint, landOwner=LandOwner.None});
+        }
+        
+        foreach(GameObject Cargo in Cargoes) {
+
+        	CargoToSpawn.Add(Cargo);
+        	
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (spawnedCargoes.Count <=1){
+        	SpawnCargo();
+        	
+        }
+
+
+    }
+
+
+
+    private void SpawnCargo() {
+
+    	
+    	removeCargoToSpawnIfAreaIsFull();
+    	for (; ; ) {
+
+    		if (spawnLocations.Count <=0 || CargoToSpawn.Count <= 0) {
+    			break;}
+
+    		int randomSpawnPoint = Random.Range(0, spawnLocations.Count);
+    		int randomCargo = Random.Range(0, CargoToSpawn.Count);
+    		if (spawnLocations[randomSpawnPoint].used== false && spawnLocations[randomSpawnPoint].landOwner== LandOwner.None) {
+    			GameObject Cargo =Instantiate(CargoToSpawn[randomCargo], spawnLocations[randomSpawnPoint].transform.position, Quaternion.identity );
+    			spawnedCargoes.Add(Cargo);
+    			EmptyAllSpawnPointsExceptCargoareas();
+    			spawnLocations[randomSpawnPoint].used =true;
+    			break;
+
+    		}
+    		
+    	}
+    }
+
+    void removeCargoToSpawnIfAreaIsFull() {
+
+    	foreach(GameObject CargoArea in CargoAreas) {
+    		GoalAreaController CargoTruck = CargoArea.GetComponent<GoalAreaController>();
+    		if (CargoTruck.isFull) {
+                
+    			foreach(GameObject Cargo in CargoToSpawn.ToList())  {
+
+    				if (Cargo.gameObject.name == CargoTruck.CargoTag) {
+    					CargoToSpawn.Remove(Cargo);
+
+    				}  } 
+    		}
+    	}
+
+    }
+
+    void EmptyAllSpawnPointsExceptCargoareas() {
+
+    	foreach (SpawnLocation spawnLocation in spawnLocations ) 
+    	{
+    		if (spawnLocation.landOwner != LandOwner.CargoArea) {
+
+    			spawnLocation.landOwner = LandOwner.None;
+    			spawnLocation.used = false;
+
+    		}
+    	}
+
+
+    }
+
+
+    public void randomlyPlaceCargoAreas() {
+
+    	foreach(GameObject CargoArea in CargoAreas) {
+
+    		for (; ; ) {
+
+    			int randomSpawnLocation = Random.Range(0, spawnLocations.Count);
+    			if (spawnLocations[randomSpawnLocation].used==false )
+    			{
+    				agent.transform.position= spawnLocations[randomSpawnLocation].transform.position;
+    				spawnLocations[randomSpawnLocation].used= true;
+    				spawnLocations[randomSpawnLocation].landOwner= LandOwner.CargoArea;
+    				break;
+    			}
+    		}
+    	}
+    }
+
+
+public void MoveAgentToRandomLocation() {
+
+	for (; ; ) { 
+		int randomSpawnLocation = Random.Range(0, spawnLocations.Count);
+
+		if (spawnLocations[randomSpawnLocation].used == false ) 
+		{
+			agent.transform.position = spawnLocations[randomSpawnLocation].transform.position;
+			spawnLocations[randomSpawnLocation].used=true;
+			break;
+
+		}
+	
+	}
+
+}
+
+    public void despawnAllCargoes(){
+    	foreach (GameObject Cargo in spawnedCargoes) {
+
+    		Destroy(Cargo);
+    	}
+    	spawnedCargoes.Clear();
+
+    }
+
+    public void ResetCargoTrucks() {
+
+    	foreach(GoalAreaController CargoTruck in CargoAreas.Select(CargoArea => CargoArea.GetComponent<GoalAreaController>())) 
+    	{
+    		CargoTruck.resetCargoTruck();
+
+    	}
+    }
+
+
+    public void RestoreCargoToSpawn()
+    {
+    	CargoToSpawn.Clear();
+    	foreach(GameObject Cargo in Cargoes)
+    	{
+    		CargoToSpawn.Add(Cargo);
+    	}
+    }
+
+    public void EmptySpawnPoints() {
+
+    	foreach(SpawnLocation spawnLocation in spawnLocations) {
+    		spawnLocation.used = false;
+    		spawnLocation.landOwner= LandOwner.None;
+    	}
+    }
+
+    public bool AllCargoAreasFilled() {
+
+    	return CargoAreas.All(CargoArea =>CargoArea.gameObject.GetComponent<GoalAreaController>().isFull);
+    }
+
+}
---
+++ UnitySDK/Assets/ML-Agents/Projects/CargoManagement/Scripts/TruckController.cs
@@ -0,0 +1,195 @@
+﻿/*using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Axel
+{
+    Front,
+    Rear
+}
+
+[Serializable]
+public struct Wheel
+{
+    public GameObject model;
+    public WheelCollider collider;
+    public Axel axel;
+}
+
+public class TruckController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float maxAcceleration = 20.0f;
+    [SerializeField]
+    private float turnSensitivity = 1.0f;
+    [SerializeField]
+    private float maxSteerAngle = 45.0f;
+    [SerializeField]
+    private Vector3 _centerOfMass;
+    [SerializeField]
+    private List<Wheel> wheels;
+
+    private float inputX, inputY;
+
+    private Rigidbody _rb;
+
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.centerOfMass = _centerOfMass;
+    }
+
+    
+    private void Update()
+    {
+        AnimateWheels();
+        GetInputs();
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void GetInputs()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.collider.motorTorque = inputY * maxAcceleration * 500 * Time.deltaTime;
+
+        }
+    }
+
+    private void Turn()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {	
+            	print("yes!!!");
+                var _steerAngle = inputX * turnSensitivity * maxSteerAngle;
+                print(_steerAngle);
+                wheel.collider.steerAngle = Mathf.Lerp(wheel.collider.steerAngle,_steerAngle,0.5f);
+            }
+        }
+    }
+
+    private void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            Quaternion _rot;
+            Vector3 _pos;
+            wheel.collider.GetWorldPose(out _pos, out _rot);
+            wheel.model.transform.position = _pos;
+            wheel.model.transform.rotation = _rot;
+        }
+    }
+}
+
+
+/*using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum Axel{
+	Front,
+	Rear
+}
+
+[Serializable]
+public struct Wheel {
+
+	public GameObject model;
+	public WheelCollider collider;
+	public Axel Axel;
+}
+
+
+public class TruckController : MonoBehaviour
+{
+	[SerializeField]
+	private float maxAcceleration = 20.0f;
+	[SerializeField]
+	private float turnSensitivity = 1.0f;
+	[SerializeField]
+	private float maxSteerAngle = 45.0f;
+	[SerializeField]
+	private List<Wheel> Wheels;
+	[SerializeField]
+	private Vector3 centerOfMass;
+
+	private float inputX, inputY;
+	private Rigidbody rb; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        AnimateWheels();
+    }
+
+    void GetInputs() {
+
+    	inputX = Input.GetAxis("Horizontal");
+    	inputY = Input.GetAxis("Vertical");
+    }
+
+    private void LateUpdate(){
+    	//Move();
+    	Turn();
+     }
+
+     private void Move(){
+
+     	foreach(var wheel in Wheels )
+     	{
+     		wheel.collider.motorTorque = inputY * maxAcceleration * 5000 * Time.deltaTime;
+     	}
+     }
+
+     private void Turn() {
+     	foreach (var wheel in Wheels){
+     		if (wheel.Axel == Axel.Front )
+     		{
+     			var _steerAngle = inputX * turnSensitivity * maxSteerAngle;
+     			wheel.collider.steerAngle = _steerAngle; //Mathf.Lerp(wheel.collider.steerAngle, _steerAngle, 0.5f);
+     		}
+     	}
+     }
+
+     private void AnimateWheels(){
+     	foreach (var wheel in Wheels) {
+
+     		Quaternion _rot;
+     		Vector3 _pos; 
+     		wheel.collider.GetWorldPose(out _pos, out _rot);
+     		wheel.model.transform.position = _pos;
+     		wheel.model.transform.rotation = _rot;
+     	}
+     }
+
+}
+*/
